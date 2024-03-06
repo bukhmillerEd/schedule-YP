9d93baca367f278c22f207276c6f250537162e06
Eduard Bukhmiller <bukhmillere@gmail.com>
2024-03-06 22:41:30 +0300

    inition

---
+++ schedule_YPApp.swift
@@ -0,0 +1,17 @@
+//
+//  schedule_YPApp.swift
+//  schedule-YP
+//
+//  Created by Эдуард Бухмиллер on 06.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct schedule_YPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
